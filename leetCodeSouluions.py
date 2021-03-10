aed3270a8bf4e04d149552e1e1aeece22c3dadbc
Narashi <meoww@outlook.com>
2021-03-10 17:54:16 +0600

    14th problem solved

--- leetCodeSouluions.py
+++ leetCodeSouluions.py
@@ -191,7 +191,22 @@
 
 
 #14th problem
-#status: not solved
+#status: solved
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        pass
+        if len(strs) == 0:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        # set prefix as the first element in the list
+        prefix = strs[0]
+        prefix_length = len(prefix)
+        # iterate and modify the prefix as per the element in the list
+        for i in strs[1:]:
+            while prefix!=i[0:prefix_length]:
+                prefix = prefix[0:prefix_length-1]
+                prefix_length = prefix_length-1
+                
+                if len(prefix) == 0:
+                    return ""
+        return prefix
